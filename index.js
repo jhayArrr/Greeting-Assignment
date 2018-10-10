e8c843187c2a49fbbb09005d3a4a90acc5bb3d32
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-10 13:39:40 -0600

    changes

---
+++ index.js
@@ -0,0 +1,5 @@
+let hour = 10; //Change this simulating automatically reading time from Internet Clock or System Tray Clock
+if (1, 12) console.log('Good Morning');
+else if (13, 17) console.log('Good Afternoon');
+else if (17, 24) console.console.log('Good Evening');
+else console.console.log(time);
